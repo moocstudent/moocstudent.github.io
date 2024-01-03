6fd8d94c701d935aec61c10f972a202a48db7879
moocstudent <deadzq@qq.com>
2024-01-03 16:58:50 +0800

    index gaussian layui msg() layui animation

---
+++ games/speed-match/godb/godbManager.js
@@ -0,0 +1,14 @@
+// import GoDB from 'godb';
+//
+// const speedMatchDB = new GoDB('speed-match');
+// const player = speedMatchDB.table('player');
+//
+// const data = {
+//     name: 'luke',
+//     age: 22
+// };
+//
+// // player.add(data) // 增
+// //     .then(luke => user.get(luke.id)) // 查，等价于 user.get({ id: luke.id })
+// //     .then(luke => user.put({ ...luke, age: 23 })) // 改
+// //     .then(luke => user.delete(luke.id)); // 删
